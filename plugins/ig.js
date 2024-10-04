bad3b959efc9c1c3d588ccbe6601af8119b66f01
EdderBot02 <107139979+EdderBot02@users.noreply.github.com>
2024-10-04 08:30:10 -0600

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -12,7 +12,7 @@ await m.react('✖️')
 }}
 handler.help = ['instagram *<link ig>*']
 handler.tags = ['downloader']
-handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3|facebook|fb|tiktok|tt)$/i
+handler.command = /^(instagramdl|instagram|igdl|ig|tiktok|tt)$/i
 export default handler
 
 
