61524399dfdb1c2d2f32a416dcc09a3ab24be8d3
EdderBot02 <107139979+EdderBot02@users.noreply.github.com>
2024-10-11 19:12:13 -0600

    Add files via upload

---
+++ plugins/mediaFire.js
@@ -0,0 +1,115 @@
+import axios from 'axios';
+import fetch from 'node-fetch';
+import cheerio from 'cheerio';
+import puppeteer from 'puppeteer-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) return m.reply(`𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝙑𝘼𝙇𝙄𝘿𝙊 𝘿𝙀 𝙈𝙀𝘿𝙄𝘼𝙁𝙄𝙍𝙀.\n\n𝙀𝙉𝙏𝙀𝙍 𝘼 𝙑𝘼𝙇𝙄𝘿 𝙈𝙀𝘿𝙄𝘼𝙁𝙄𝙍𝙀 𝙇𝙄𝙉𝙆`)
+try {  
+
+await m.react('⌛')
+let res = await mediafireDl(args[0])
+//let { name, size, date, mime, link } = res
+let caption = `╔═.✵.══════════╗
+┃ ✫彡 *ミ★ 𝘌𝘯𝘪𝘨𝘮𝘢-𝘉𝘰𝘵 ★彡*
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ ✫彡𝙉𝙊𝙈𝘽𝙍𝙀 | 𝙉𝘼𝙈𝙀
+┃ ${res.filename}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ ✫彡 𝙋𝙀𝙎𝙊 |  𝙎𝙄𝙕𝙀
+┃ ${res.filesizeH}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ ✫彡 𝙏𝙄𝙋𝙊 | 𝙏𝙔𝙋𝙀
+┃ ${res.ext}
+╚══════════.✵.═╝`.trim()
+
+//let url="https://download2389.mediafire.com/tg8moatxwpfgtAn64kdPUK5dJjnEW0TMdOYY20xulCdj5CqFr8PBNStEqzlzrZMCLCMyaXTX90Z_fdgRU44E8ZKytt8EDoBLGchw9z5V8G1k-jsEbi2vBmLGdD3nRz7VPh7M-BRyCoMSgy-0GSKCXB14E3CQq5PSgv1MeQ8rvThOyJo/fbdf2aydm7mbwoo/Waifu-Hub-15-season-1-modmenus.com.apk"
+await m.reply(caption)
+if (res.filesizeH.includes('GB') && parseFloat(res.filesizeH.replace('GB', '')) > 1.79) return await m.reply('El archivo no debe pesar mas de 2 GB')
+await conn.sendFile(m.chat, res.url, res.filename, '', m, null, {mimetype: res.ext, asDocument: true});
+await m.react('✅')
+return
+} catch (e) {
+await m.react('❌')
+}
+}
+handler.help = ['mediafire'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(mediafire|mediafiredl|dlmediafire)$/i
+
+export default handler
+
+
+async function mediafireDl(urldl) {
+puppeteer.use(StealthPlugin())
+  const browser = await puppeteer.launch({
+     args: ["--no-sandbox","--disabled-setupid-sandbox"]
+});
+
+  const page = await browser.newPage();
+  await page.goto(urldl);
+  const html = await page.content();
+  await browser.close();
+  const $ = cheerio.load(html);
+  const url = $('#downloadButton').attr('href');
+  const filename = $('body > main > div.content > div.center > div > div.dl-btn-cont > div.dl-btn-labelWrap > div.promoDownloadName.notranslate > div').attr('title').replaceAll(' ', '').replaceAll('\n', '');
+  const date = $('body > main > div.content > div.center > div > div.dl-info > ul > li:nth-child(2) > span').text();
+  const filesizeH = $('#downloadButton').text().replace('Download', '').replace('(', '').replace(')', '').replace('\n', '').replace('\n', '').replace('                         ', '').replaceAll(' ', '');
+  let ext = '';
+  const rese = await axios.head(url);
+  ext = rese.headers['content-type'];
+  return {filename, filesizeH, date, ext, url};  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import axios from 'axios';
+import fetch from 'node-fetch';
+import cheerio from 'cheerio';
+import {mediafiredl} from '@bochilteam/scraper';
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) return m.reply(`𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝙑𝘼𝙇𝙄𝘿𝙊 𝘿𝙀 𝙈𝙀𝘿𝙄𝘼𝙁𝙄𝙍𝙀.\n\n𝙀𝙉𝙏𝙀𝙍 𝘼 𝙑𝘼𝙇𝙄𝘿 𝙈𝙀𝘿𝙄𝘼𝙁𝙄𝙍𝙀 𝙇𝙄𝙉𝙆`)
+try {  
+await m.react('⌛')
+let res = await mediafiredl(args[0])
+//let { name, size, date, mime, link } = res
+let caption = `╔═.✵.══════════╗
+┃ ✫彡 *Enigma-Bot ミ⁠●⁠﹏⁠☉⁠ミ*
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ ✫彡𝙉𝙊𝙈𝘽𝙍𝙀 | 𝙉𝘼𝙈𝙀
+┃ ${res.filename}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ ✫彡 𝙋𝙀𝙎𝙊 |  𝙎𝙄𝙕𝙀
+┃ ${res.filesizeH}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ ✫彡 𝙏𝙄𝙋𝙊 | 𝙏𝙔𝙋𝙀
+┃ ${res.ext}
+╚══════════.✵.═╝`.trim()
+
+await m.reply(caption)
+if (res.filesizeH.includes('GB') && parseInt(res.filesizeH.replace('GB', '')) > 1.8) return await m.sendMessage(m.chat, {text: 'El archivo no debe pesar mas de 2 GB'}, {quoted: m})
+await conn.sendFile(m.chat, res.url, res.filename, '', m, null, {mimetype: res.ext, asDocument: true});
+await m.react('✅')
+} catch (e) {
+await m.react('❌')
+}}
+handler.help = ['mediafire'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(mediafire|mediafiredl|dlmediafire)$/i
+
+export default handler
+
+
+*/
---
+++ plugins/ytmp3.js
@@ -0,0 +1,77 @@
+import axios from 'axios';
+import yts from 'yt-search'
+
+let handler = async (m, { command, args, text, usedPrefix}) => {
+    if (!args[0]) return m.reply("Ingresa el enlace del vídeo de YouTube")
+    try { 
+        await m.react('⌛')
+        let enlace=`${args[0]}`
+const regexEnlaceYoutube=/^(https?:\/\/)?(www\.)?(m\.)?(youtube\.com|youtu\.be)\/(shorts\/|watch\?v=)?([a-zA-Z0-9_-]{11})(\?.*)?$/
+//        const regexEnlaceYoutube =/^(https?:\/\/)?(www\.)?(m\.)?(youtube\.com|youtu\.be)\/(watch\?v=)?([a-zA-Z0-9_-]{11})(\?.*)?$/
+
+       // const regexEnlaceYoutube = /^(https?:\/\/)?(www\.)?(m\.)?(youtube\.com|youtu\.be)\/(watch\?v=)?([a-zA-Z0-9_-]{11})$/;
+        if(!(regexEnlaceYoutube.test(enlace)))
+        {
+console.log("Se busco resultado")
+          const results = await search(`${text}`)
+          let img = await (await fetch(`${results[0].thumbnail}`)).buffer()
+          let txt = '❍⌇─➭ *Youtube-Downloader* «•«━┑\n'
+              txt += `	➠  *Titulo* : ${results[0].title}\n`
+              txt += `	➠  *Duración* : ${results[0].timestamp}\n`
+              txt += `	➠  *Publicado* : ${results[0].ago}\n`
+              txt += `	➠  *Autor* : ${results[0].author.name}\n`
+              txt += `	➠  *Url* : ${results[0].url}\n\n⋘ 𝑃𝑙𝑒𝑎𝑠𝑒 𝑤𝑎𝑖𝑡... ⋙`
+          await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null)
+          enlace=results[0].url
+        }
+        const yt = await ytmp3(enlace)
+       // await delay(3 * 1000)
+        await conn.sendMessage(m.chat, { audio: { url:yt }, mimetype: 'audio/mpeg'} , { quoted: m })   
+        await m.react('✅')
+    }
+    catch(e) {
+        await m.react('❌')
+    }
+    return true
+}
+
+handler.command = /^(play|ytpro|yt(a|mp3)|ytmp3doc)$/i;
+export default handler
+
+async function ytmp3(url) {
+        let userAgent = `Mozilla/5.0 (X11; U; Linux i686; en-US; rv:0.9.3) Gecko/20010801`
+  const { data } = await axios(`https://x2download.app/api/ajaxSearch`, {
+    method: "post",
+    data: { q: url, vt: "home" },
+    headers: {
+      'User-Agent': userAgent,
+      "content-type": "application/x-www-form-urlencoded",
+      accept: "*/*",
+      "x-requested-with": "XMLHttpRequest"
+    }
+  })
+  
+  console.log(data)
+ // await delay(3 * 1000)
+  const data2= await axios(`https://backend.svcenter.xyz/api/convert-by-45fc4be8916916ba3b8d61dd6e0d6994`, {
+    method: "post",
+    data: {v_id:data.vid,ftype:"mp3",fquality:128,token:data.token,timeExpire:data.timeExpires,client:"X2Download.app"},
+    headers: {
+    'User-Agent': userAgent,
+      "Content-Type":"application/x-www-form-urlencoded; charset=UTF-8",
+      accept: "*/*",
+          "Origin":"https://x2download.app",
+  "Referer":"https://x2download.app/",
+        "X-Requested-Key":"de0cfuirtgf67a"
+    }
+  })
+  
+  return data2.data.d_url
+}
+
+async function search(query, options = {}) {
+  let search = await yts.search({ query, hl: "es", gl: "ES", ...options })
+  return search.videos
+}
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
